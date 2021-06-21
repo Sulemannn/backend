c2b7efaad9194d781878643ea85ca3d1afb4e382
Suleman <suleman.khn10@gmail.com>
2021-06-21 16:36:15 +0200

    models done

---
+++ trippy_basics_api/models/tableModel.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose")
+
+const tableSchema = new mongoose.Schema({
+    seat: Number,
+    isVIP: Boolean,
+    created: { type: Date, default: Date.now }
+})
+
+const tableModel = mongoose.model("Table", tableSchema)
+
+module.exports = { tableModel }
